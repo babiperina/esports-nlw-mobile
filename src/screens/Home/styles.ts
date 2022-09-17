432390572c344b4ae2a0dc240875b03150bcba8d
Babi Perina <bperinabezerra@gmail.com>
2022-09-16 19:53:44 -0700

    gamecard

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,18 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logo: {
+    width: 214,
+    height: 120,
+    marginTop: '25%',
+    marginBottom: 48,
+  },
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 34,
+  }
 });
